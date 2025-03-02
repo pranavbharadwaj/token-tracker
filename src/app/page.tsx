7af425a2e1f24b2738a7d29a9672bf85086892a4
pranavbharadwajs <pranavbharadwaj@signzclub.com>
2025-03-03 03:42:08 +0530

    GSAP Animation for intro, just to showcase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,47 @@ import styles from "@/styles/page.module.scss";
 import Header from "@/components/Header";
 import Main from "@/components/Main";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 export default function Home() {
   const [queryClient] = useState(() => new QueryClient());
+  const taglineRef1 = useRef(null);
+  const taglineRef2 = useRef(null);
+  const taglineRef3 = useRef(null);
+  const sectionRef = useRef(null);
+
+  useGSAP(() => {
+    gsap.to(taglineRef1.current, { autoAlpha: 1, duration: 1, delay: 1 });
+    gsap.to(taglineRef2.current, { autoAlpha: 1, duration: 1 }, ">");
+    gsap.to(taglineRef3.current, { autoAlpha: 1, duration: 1 }, ">");
+    gsap.to(
+      taglineRef1.current,
+      { autoAlpha: 0, duration: 0.33, delay: 2 },
+      ">"
+    );
+    gsap.to(taglineRef2.current, { autoAlpha: 0, duration: 0.33 }, ">");
+    gsap.to(taglineRef3.current, { autoAlpha: 0, duration: 0.33 }, ">");
+    gsap.to(sectionRef.current, { autoAlpha: 0, duration: 1 }, ">");
+  }, []);
+
   return (
     <QueryClientProvider client={queryClient}>
       <div className={styles.page}>
         <Header />
         <Main />
+        <div className={styles.intro} ref={sectionRef}>
+          <p className={styles.introTag1} ref={taglineRef1}>
+            Effortlessly track the
+          </p>
+          <p className={styles.introTag2} ref={taglineRef2}>
+            top 50 cryptocurrencies with
+          </p>
+          <p className={styles.introTag3} ref={taglineRef3}>
+            live updates and insights.
+          </p>
+        </div>
       </div>
     </QueryClientProvider>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,10 +3,10 @@ import UserLogin from "@/components/UserLogin";
 import styles from "@/styles/header.module.scss";
 import CurrencySelect from "./CurrencySelect";
 
-const Header: React.FC = () => {
+const Header: React.FC = ({ logoRef }) => {
   return (
     <header className={styles.header}>
-      <div className={styles.logo}>
+      <div className={styles.logo} ref={logoRef}>
         <span className={styles.logothinFont}>TOKEN</span>
         <span className={styles.logothickFont}>TRACK</span>
       </div>
